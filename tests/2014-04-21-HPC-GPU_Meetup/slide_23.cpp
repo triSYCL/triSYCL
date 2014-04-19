2455579d10ae857b0bdeb2a8b3589ffa9a7e504c
Ronan Keryell <Ronan.Keryell@amd.com>
2014-04-19 08:52:46 -0700

    Update slide 23 to allow a functor kernel without closure by using global variables

--- tests/2014-04-21-HPC-GPU_Meetup/slide_23.cpp
+++ tests/2014-04-21-HPC-GPU_Meetup/slide_23.cpp
@@ -1,16 +1,46 @@
+#include <CL/sycl.hpp>
+
+// To have FunctionObject working without explicit closure, put the
+// accessor as a global variable. But that means to pull many things as a
+// global variable...
+int result; //< this is where we will write our result
+// wrap our result variable in a buffer
+cl::sycl::buffer<int> resultBuf(&result, 1);
+auto writeResult = resultBuf.get_access<cl::sycl::access::write>();
+
+int main()
+{
+
+  { // by sticking all the SYCL work in a {} block, we ensure
+    // all SYCL tasks must complete before exiting the block
+
+    // create a queue to work on
+    cl::sycl::queue myQueue;
+
+    // create some ‘commands’ for our ‘queue’
+    cl::sycl::command_group(myQueue, [&]()
+    {
+      // request access to our buffer
+      writeResult = { resultBuf };
 //////// Start left side of the slide
-class FunctionObject {
-    operator(){
+struct FunctionObject {
+    void operator()() {
         writeResult [0] = 1234;
     }
 };
-single_task(FunctionObject());
+cl::sycl::single_task(FunctionObject());
 //////// End left side of the slide
 
 //////// Start right side of the slide
-single_task(kernel_lambda<class simple_test>([=] ()
+cl::sycl::single_task(cl::sycl::kernel_lambda<class simple_test>([=] ()
             {
                 writeResult [0] = 1234;
             }
     ));
 //////// End right side of the slide
+    }); // end of our commands for this queue
+
+  } // end scope, so we wait for the queue to complete
+
+  printf("Result = %d\n", result);
+}
