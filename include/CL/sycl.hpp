9373b5a3a2e1b11645cdea7fc94f219464e66f15
Ronan Keryell <Ronan.Keryell@amd.com>
2014-07-09 16:42:05 +0200

    Add stubs and documentation for storage abstraction

--- include/CL/sycl.hpp
+++ include/CL/sycl.hpp
@@ -918,6 +918,91 @@ TRISYCL_IMPL(: AccessorImpl<dataType, dimensions, mode, target>) {
 };
 
 
+/** Abstract the way storage is managed to allow the programmer to control
+    the storage management of buffers
+
+    \param T
+    the type of the elements of the underlying data
+
+    The user is responsible for ensuring that their storage class
+    implementation is thread-safe.
+*/
+template <typename T>
+struct storage {
+  /// \todo Extension to SYCL specification: provide pieces of STL
+  /// container interface?
+  using element = T;
+  using value_type = T;
+
+
+  /** Method called by SYCL system to get the number of elements of type T
+      of the underlying data
+
+      \todo This is inconsistent in the specification with get_size() in
+      buffer which returns the byte size. Is it to be renamed to
+      get_count()?
+  */
+  virtual size_t get_size() = 0;
+
+
+  /** Method called by the SYCL system to know where that data is held in
+      host memory
+
+      \return the address or nullptr if SYCL has to manage the temporary
+      storage of the data.
+  */
+  virtual T* get_host_data() = 0;
+
+
+  /** Method called by the SYCL system at the point of construction to
+      request the initial contents of the buffer
+
+      \return the address of the data to use or nullptr to skip this data
+      initialization
+  */
+  virtual const T* get_initial_data() = 0;
+
+
+  /** Method called at the point of construction to request where the
+      content of the buffer should be finally stored to
+
+      \return the address of where the buffer will be written to in host
+      memory.
+
+      If the address is nullptr, then this phase is skipped.
+
+      If get_host_data() returns the same pointer as get_initial_data()
+      and/or get_final_data() then the SYCL system should determine whether
+      copying is actually necessary or not.
+  */
+  virtual T* get_final_data() = 0;
+
+
+  /** Method called when the associated memory object is destroyed.
+
+      This method is only called once, so if a memory object is copied
+      multiple times, only when the last copy of the memory object is
+      destroyed is the destroy method called.
+
+      Exceptions thrown by the destroy method will be caught and ignored.
+  */
+  virtual void destroy() = 0;
+
+
+  /** \brief Method called when a command_group which accesses the data is
+      added to a queue
+
+     After completed is called, there may be further calls of
+      in_use() if new work is enqueued that operates on the memory object.
+  */
+  virtual void in_use() = 0;
+
+
+  /// Method called when the final enqueued command has completed
+  virtual void completed() = 0;
+};
+
+
 /** A SYCL buffer is a multidimensional variable length array (à la C99
     VLA or even Fortran before) that is used to store data to work on.
 
@@ -967,7 +1052,18 @@ struct buffer TRISYCL_IMPL(: BufferImpl<T, dimensions>) {
     Impl(host_data, r.getImpl()) {}
 
 
-    //buffer(storage<T> &store, range<dimensions> r)
+  /** Create a new buffer from a storage abstraction provided by the user
+
+      \param store is the storage back-end to use for the buffer
+
+      \param r defines the size
+
+      The storage object has to exist during all the life of the buffer
+      object.
+
+      \todo To be implemented
+  */
+  buffer(storage<T> &store, range<dimensions> r) { assert(0); }
 
 
   /** Create a new allocated 1D buffer initialized from the given elements
