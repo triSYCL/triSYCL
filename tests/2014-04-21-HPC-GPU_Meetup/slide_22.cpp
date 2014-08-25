a8d4ae6defa5cd3fb2d7f59d1b42a6775b099d51
Ronan Keryell <Ronan.Keryell@amd.com>
2014-08-25 11:51:10 -0700

    Add lit test cases to programs from 2014-04-21-HPC-GPU_Meetup

--- tests/2014-04-21-HPC-GPU_Meetup/slide_22.cpp
+++ tests/2014-04-21-HPC-GPU_Meetup/slide_22.cpp
@@ -1,4 +1,9 @@
+/* RUN: %{execute}%s | %{filecheck} %s
+   CHECK: data[5] = 10, should be 10
+*/
 #include <vector>
+#include <algorithm>
+#include <iostream>
 #include <CL/sycl.hpp>
 
 using namespace cl::sycl;
@@ -7,6 +12,8 @@ int main() {
   queue my_queue;
   const int size = 10;
   int data[size];
+  // Initialize the data with the index value
+  std::iota(std::begin(data), std::end(data), 0);
   const int groupsize = 2;
 //////// Start slide
 buffer<int> my_buffer(data, size);
@@ -26,5 +33,6 @@ command_group(my_queue, [&]()
 	}));
 });
 //////// End slide
-    return 0;
+  std::cout << "data[5] = " << data[5] << ", should be 10" << std::endl;
+  return 0;
 }
