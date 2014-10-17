63d558be9f20597d1c3c37cc081148aca6f95b24
Ronan Keryell <Ronan.Keryell@amd.com>
2014-10-17 16:13:32 -0700

    Add scalar expansion to cl::sycl::vec to allow initialization from a scalar

--- include/CL/sycl.hpp
+++ include/CL/sycl.hpp
@@ -1448,15 +1448,16 @@ struct vec;
 #endif
 
 
-  /** Construct an array from initializer elements provided as a tuple
+  /** Helper to construct an array from initializer elements provided as a
+      tuple
 
       The trick is to get the std::index_sequence<> that represent 0,
        1,..., dimension-1 as a variadic template pack Is that we can
-       iterate on in the function,
-   */
+       iterate on, in this function.
+  */
   template <typename V, typename Tuple, size_t... Is>
   std::array<typename V::element_type, V::dimension>
-  tuple_to_array(Tuple && t, std::index_sequence<Is...>) {
+  tuple_to_array_iterate(Tuple t, std::index_sequence<Is...>) {
     /* The effect is like a static for loop with Is counting from 0 to
        dimension-1 and thus constructing a uniform initialization { }
        construction from each tuple element */
@@ -1464,19 +1465,90 @@ struct vec;
   }
 
 
-  /**
+  /** Construct an array from initializer elements provided as a tuple
    */
   template <typename V, typename Tuple>
-  std::array<typename V::element_type, V::dimension>
-  expand(Tuple && t) {
-    return tuple_to_array<V>(t/*std::forward(t)*/,
-                             std::make_index_sequence<std::tuple_size<Tuple>::value>{});
+  auto tuple_to_array(Tuple t) {
+    /* Construct an index_sequence with 0, 1, ..., (size of the tuple-1)
+       so that tuple_to_array_iterate can statically iterate on it */
+    return tuple_to_array_iterate<V>(t,
+                                     std::make_index_sequence<std::tuple_size<Tuple>::value>{});
+  }
+
+
+  /** Allows optional expansion of a 1-element tuple to a V::dimension
+      tuple to replicate scalar values in vector initialization
+  */
+  template <typename V, typename Tuple, bool expansion = false>
+  struct expand_to_vector {
+    static_assert(V::dimension == std::tuple_size<Tuple>::value,
+                  "The number of element in initialization should match the dimension of the vector");
+
+    // By default, act as a pass-through and do not do any expansion
+    static auto expand(Tuple t) { return t; }
+
+  };
+
+
+  /** Specialization in the case we ask for expansion */
+  template <typename V, typename Tuple>
+  struct expand_to_vector<V, Tuple, true> {
+    static_assert(std::tuple_size<Tuple>::value == 1,
+                  "Since it is a vector initialization from a scalar there should be only one initializer value");
+
+
+    /** Construct a tuple from a value
+
+        @param value is used to initialize each tuple element
+
+        @param size is the number of elements of the tuple to be generated
+
+        The trick is to get the std::index_sequence<> that represent 0,
+        1,..., dimension-1 as a variadic template pack Is that we can
+        iterate on, in this function.
+    */
+    template <typename Value, size_t... Is>
+    static auto fill_tuple(Value e, std::index_sequence<Is...>) {
+      /* The effect is like a static for loop with Is counting from 0 to
+         dimension-1 and thus replicating the pattern to have
+         make_tuple( (0, e), (1, e), ... (n - 1, e) )
+
+         Since the "," operator is just to throw away the Is value (which
+         is needed for the pack expansion), at the end this is equivalent
+         to:
+         make_tuple( e, e, ..., e )
+      */
+      return std::make_tuple(((void)Is, e)...);
+    }
+
+
+    /** We expand the 1-element tuple by replicating into a tuple with the
+        size of the vector */
+    static auto expand(Tuple t) {
+      return fill_tuple(std::get<0>(t),
+                        std::make_index_sequence<V::dimension>{});
+    }
+
+  };
+
+  /** Create the array data of V from a tuple of initializer
+
+      If there is only 1 initializer, this is a scalar initialization of a
+      vector and the value is expanded to all the vector elements first.
+   */
+  template <typename V, typename Tuple>
+  auto expand(Tuple t) {
+    return tuple_to_array<V>(expand_to_vector<V,
+                             decltype(t),
+                             /* Only ask the expansion to all vector
+                                element if there only a scalar
+                                initializer */
+                             std::tuple_size<Tuple>::value == 1>{}.expand(t));
   }
 
 
   /** Flattening helper that does not change scalar values but flatten a
       vec<T, n> v into a tuple<T, T,..., T>{ v[0], v[1],..., v[n-1] }
-
 
       If we have a vector, just forward its array content since an array
       has also a tuple interface :-) (23.3.2.9 Tuple interface to class
@@ -1489,23 +1561,25 @@ struct vec;
     return i.data;
   }
 
+
   /** If we do not have a vector, just forward it as a tuple up to the
       final initialization.
 
       @return typically tuple<double>{ 2.4 } from 2.4 input for example
   */
   template <typename V, typename Type>
-  auto flatten(Type && i) {
+  auto flatten(const Type i) {
     return std::forward_as_tuple(i);
   }
 
 
-  /** Take some initializer values and apply flattening on each
+  /** Take some initializer values and apply flattening on each value
 
       @return a tuple of scalar initializer values
    */
   template <typename V, typename... Types>
-  auto flatten_to_tuple(Types &&... i) {
+  auto flatten_to_tuple(const Types... i) {
+    // Concatenate the tuples returned by each flattening
     return std::tuple_cat(flatten<V>(i)...);
   }
 
