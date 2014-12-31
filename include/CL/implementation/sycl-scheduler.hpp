f054057eec5df7770f6f7fd5d910e56f67c6718e
Ronan Keryell <Ronan.Keryell@amd.com>
2014-12-30 17:11:18 -0800

    Start asynchronous implementation with tasks
    Execute a single task in a... thread task.

---
+++ include/CL/implementation/sycl-scheduler.hpp
@@ -0,0 +1,21 @@
+#include <thread>
+
+namespace cl {
+namespace sycl {
+namespace trisycl {
+
+struct Task {
+  /// Add a new task to the task graph
+  static void add(std::function<void(void)> f) {
+    // Execute the functor in a new thread
+    std::thread thread(f);
+    // std::cout << thread.get_id();
+    // For now just wait for the synchronous execution
+    thread.join();
+  }
+
+};
+
+}
+}
+}
