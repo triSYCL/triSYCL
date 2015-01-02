29375aa702aa4ba7331a14e394493031b84ea095
Ronan Keryell <Ronan.Keryell@amd.com>
2015-01-01 18:34:51 -0800

    Simplify the command_group to keep track directly of the current Task

--- include/CL/implementation/sycl-scheduler.hpp
+++ include/CL/implementation/sycl-scheduler.hpp
@@ -8,44 +8,47 @@ namespace trisycl {
 
 struct Task {
   /// Add a new task to the task graph
-  static void add(std::function<void(void)> f) {
+  static void add(std::function<void(void)> F) {
 #if TRISYCL_ASYNC
     // Execute the functor in a new thread
-    std::thread thread(f);
+    std::thread thread(F);
     // std::cout << thread.get_id();
     // For now just wait for the synchronous execution
     thread.join();
 #else
     // Just a synchronous execution
-    f();
+    F();
 #endif
   }
 
 };
 
 
-/** Store the current CommandGroupImpl to attach the task and accessors to it.
+/** Store the current Task to attach the task and accessors to it.
 
     Use a TLS variable since there may be several threads in the program.
 
     \todo Change the specification to avoid this hack.
 */
-thread_local CommandGroupImpl *current_command_group = nullptr;
+thread_local std::shared_ptr<Task> CurrentTask;
 
 
-struct CommandGroupImpl {
+struct CommandGroupImpl : public Debug<CommandGroupImpl> {
+
+
   template <typename Functor>
   CommandGroupImpl(queue Q, Functor F) {
-    // Nesting of command_group is forbidden
-    assert(current_command_group == nullptr);
-    current_command_group = this;
+    // Nesting of command_group is forbidden, so there should be no
+    // current task yet
+    assert(!CurrentTask);
+    CurrentTask = std::make_shared<Task>();
     F();
   }
 
 
   ~CommandGroupImpl() {
     // Reset the current_command_group at the end of the command_group
-    current_command_group = nullptr;
+    CurrentTask.reset();
   }
 
 };
