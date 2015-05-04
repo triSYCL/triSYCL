9c37278db79bf1655ebc85af1bf7ad181836dd0d
Ronan Keryell <Ronan.Keryell@amd.com>
2015-05-04 10:15:03 -0700

    Update device_selector to final specification

--- include/CL/sycl/device_selector.hpp
+++ include/CL/sycl/device_selector.hpp
@@ -22,23 +22,101 @@ namespace sycl {
 
     The device with the highest score is selected
 */
-struct device_selector {
-  // The user-provided operator computing the score
-  virtual int operator() (device dev) = 0;
+class device_selector {
+
+public:
+
+  /** Returns a selected device using the functor operator defined in
+      sub-classes operator()(const device &dev)
+
+      \todo To be implemented
+  */
+  device select_device() const {
+    assert(0);
+    return {};
+  }
+
+
+  /**  This pure virtual operator allows the customization of device
+       selection.
+
+       It defines the behavior of the device_selector functor called by
+       the SYCL runtime on device selection. It returns a "score" for each
+       device in the system and the highest rated device will be used
+       by the SYCL runtime.
+  */
+  virtual int operator() (const device &dev) const = 0;
 };
 
 
-/** Select the best GPU, if any
+/** Devices selected by heuristics of the system
+
+    If no OpenCL device is found then it defaults to the SYCL host device.
+
+    \todo to be implemented
+
+    \todo to be named device_selector::default instead in the specification?
+*/
+class default_selector : public device_selector {
+
+public:
+
+  // The user-provided operator computing the score
+  int operator() (const device &dev) const override { return 1; }
+};
+
+
+/** Select devices according to device type info::device::device_type::gpu
+    from all the available OpenCL devices.
+
+    If no OpenCL GPU device is found the selector fails.
+
+    Select the best GPU, if any.
 
     \todo to be implemented
 
     \todo to be named device_selector::gpu instead in the specification?
-
-    \todo it is named opencl_gpu_selector
 */
-struct gpu_selector : device_selector {
+class gpu_selector : public device_selector {
+
+public:
+
   // The user-provided operator computing the score
-  int operator() (device dev) override { return 1; }
+  int operator() (const device &dev) const override { return 1; }
+};
+
+
+/** Select devices according to device type info::device::device_type::cpu
+    from all the available devices and heuristics
+
+    If no OpenCL CPU device is found the selector fails.
+
+    \todo to be implemented
+
+    \todo to be named device_selector::cpu instead in the specification?
+*/
+class cpu_selector : public device_selector {
+
+public:
+
+  // The user-provided operator computing the score
+  int operator() (const device &dev) const override { return 1; }
+};
+
+
+/** Selects the SYCL host CPU device that does not require an OpenCL
+    runtime
+
+    \todo to be implemented
+
+    \todo to be named device_selector::host instead in the specification?
+*/
+class host_selector : public device_selector {
+
+public:
+
+  // The user-provided operator computing the score
+  int operator() (const device &dev) const override { return 1; }
 };
 
 /// @} to end the execution Doxygen group
