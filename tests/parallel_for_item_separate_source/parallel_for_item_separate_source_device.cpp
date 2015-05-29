07c38dafd2171e44394106b85dde1e71e05b7c76
Lee Howes <lhowes@qti.qualcomm.com>
2015-05-28 17:05:45 -0700

    Added sketch of a scalar entrypoint version of the device code.

--- tests/parallel_for_item_separate_source/parallel_for_item_separate_source_device.cpp
+++ tests/parallel_for_item_separate_source/parallel_for_item_separate_source_device.cpp
@@ -6,7 +6,7 @@
 
 constexpr size_t N = 5;
 
-//[[OpenCLKernel]] 
+//extern "OpenCL"
 void myKernel(
     cl::sycl::nd_item<1> index,
     cl::sycl::device_queue dq,
@@ -14,16 +14,17 @@ void myKernel(
     int *output
     )
 {
+
     // TODO: 
     // Allow grabbing default queue from item?
 
-    using namespace cl::sycl;    
+    using namespace cl::sycl;
     output[index.get_global()[0]] = static_cast<int>(index.get_global()[0]) + addValue;
 
     handler_event first_event = dq.submit([&](device_handler &cgh) {
         // Make us run after parent kernel
         auto acc = index.get_parent_event().get_access();
-        cgh.parallel_for(nd_range<1> {N-1, 1},
+        cgh.parallel_for(nd_range<1> {N - 1, 1},
             [=](nd_item<1> index) {
             output[index.get_global()[0] + 1] = 5;
         });
@@ -32,9 +33,49 @@ void myKernel(
     dq.submit([&](device_handler &cgh) {
         // Make second kernel dependent on first
         auto acc = first_event.get_access();
-        cgh.parallel_for(nd_range<1> {N-2, 1},
+        cgh.parallel_for(nd_range<1> {N - 2, 1},
             [=](nd_item<1> index) {
             output[index.get_global()[0] + 2] = 6;
         });
     });
+}
+
+// Experimental direct call version with parallel_for embedded in the function body
+// Probably wouldn't want cgh to appear here
+//extern "OpenCL"
+void myKernel2(
+    cl::sycl::handler cgh,
+    cl::sycl::nd_range<1> range,
+    cl::sycl::device_queue dq,
+    int addValue,
+    int *output
+    )
+{
+    cgh.parallel_for(range, [&](cl::sycl::nd_item<1> index)
+    {
+
+        // TODO: 
+        // Allow grabbing default queue from item?
+
+        using namespace cl::sycl;
+        output[index.get_global()[0]] = static_cast<int>(index.get_global()[0]) + addValue;
+
+        handler_event first_event = dq.submit([&](device_handler &cgh) {
+            // Make us run after parent kernel
+            auto acc = index.get_parent_event().get_access();
+            cgh.parallel_for(nd_range<1> {N - 1, 1},
+                [=](nd_item<1> index) {
+                output[index.get_global()[0] + 1] = 5;
+            });
+        });
+
+        dq.submit([&](device_handler &cgh) {
+            // Make second kernel dependent on first
+            auto acc = first_event.get_access();
+            cgh.parallel_for(nd_range<1> {N - 2, 1},
+                [=](nd_item<1> index) {
+                output[index.get_global()[0] + 2] = 6;
+            });
+        });
+});
 }
--- tests/parallel_for_item_separate_source/parallel_for_item_separate_source_host.cpp
+++ tests/parallel_for_item_separate_source/parallel_for_item_separate_source_host.cpp
@@ -14,6 +14,13 @@ void myKernel(
     cl::sycl::device_queue,
     int,
     int *);
+void myKernel2(
+    cl::sycl::handler cgh,
+    cl::sycl::nd_range<1> range,
+    cl::sycl::device_queue dq,
+    int addValue,
+    int *output
+    );
 
 int main() 
 {    
@@ -36,6 +43,19 @@ int main()
             );
         });
         VERIFY_BUFFER_VALUE(a, [](id<1> i) { return ((i == 0)?i[0]:((i == 1) ? 5 : 6)); });
+
+        // Test interface to show direct kernel call version
+        // We would actually load this through a cl::Kernel as we do for the above one
+        // which would hide away the pointer conversion such that acc could be passed directly
+        // and cgh might or might not be passed... 
+        // cgh.run(kernelObject2(dq, 0, acc)); Would be another way to structure it
+        myQueue.submit([&](handler &cgh) {
+            auto acc = a.get_access<access::write>(cgh);
+            auto dq = cgh.get_device_queue();
+            // Capture arguments for kernel object
+            myKernel2(cgh, nd_range<1> {N, 1}, dq, 0, acc.get_pointer());
+        });
+        VERIFY_BUFFER_VALUE(a, [](id<1> i) { return ((i == 0) ? i[0] : ((i == 1) ? 5 : 6)); });
     }
     return 0;
 }
