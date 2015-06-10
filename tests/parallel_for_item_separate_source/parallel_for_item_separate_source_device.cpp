7361d1690ac7ad7f9b02ecb1a02bd0f794c36517
Lee Howes <lhowes@qti.qualcomm.com>
2015-06-10 14:00:05 -0700

    Renaming and example for per-work-group kernel representation.

--- tests/parallel_for_item_separate_source/parallel_for_item_separate_source_device.cpp
+++ tests/parallel_for_item_separate_source/parallel_for_item_separate_source_device.cpp
@@ -7,7 +7,7 @@
 constexpr size_t N = 5;
 
 //extern "OpenCL"
-void myKernel(
+void myKernelTraditionalWorkItemEntryPoint(
     cl::sycl::nd_item<1> index,
     cl::sycl::device_queue dq,
     int addValue,
@@ -43,7 +43,7 @@ void myKernel(
 // Experimental direct call version with parallel_for embedded in the function body
 // Probably wouldn't want cgh to appear here
 //extern "OpenCL"
-void myKernel2(
+void myKernelKernelEntryPoint(
     cl::sycl::handler cgh,
     cl::sycl::nd_range<1> range,
     cl::sycl::device_queue dq,
@@ -51,6 +51,9 @@ void myKernel2(
     int *output
     )
 {
+    // This code is scalar for the entire kernel
+
+    // Parallel for for the entire NDRange
     cgh.parallel_for(range, [&](cl::sycl::nd_item<1> index)
     {
 
@@ -78,4 +81,51 @@ void myKernel2(
             });
         });
 });
+}
+
+// Experimental scalar per-work-group version
+// Passed range is the WG size
+//extern "OpenCL"
+void myKernelWGEntryPoint(
+  cl::sycl::handler cgh,
+  cl::sycl::range<1> range,
+  cl::sycl::item<1> groupIndex,
+  cl::sycl::device_queue dq,
+  int addValue,
+  int *output
+  )
+{
+  // This code is scalar for the current work-group
+  // This means that local declarations can *only* go here
+  // and local construction is achieved per-work-group in the 
+  // obvious way
+
+  // Parallel for for the work items in the work-group
+  cgh.parallel_for(range, [&](cl::sycl::item<1> index)
+  {
+
+    // TODO: 
+    // Allow grabbing default queue from item?
+
+    using namespace cl::sycl;
+    output[index.get()[0]*groupIndex.get_range()[0]+index.get(0)] = static_cast<int>(index.get()[0] * groupIndex.get_range()[0] + index.get(0)) + addValue;
+
+    handler_event first_event = dq.submit([&](device_handler &cgh) {
+      // Make us run after parent kernel
+      auto acc = index.get_parent_event().get_access();
+      cgh.parallel_for(nd_range<1> {N - 1, 1},
+        [=](nd_item<1> index) {
+        output[index.get_global()[0] + 1] = 5;
+      });
+    });
+
+    dq.submit([&](device_handler &cgh) {
+      // Make second kernel dependent on first
+      auto acc = first_event.get_access();
+      cgh.parallel_for(nd_range<1> {N - 2, 1},
+        [=](nd_item<1> index) {
+        output[index.get_global()[0] + 2] = 6;
+      });
+    });
+  });
 }
--- tests/parallel_for_item_separate_source/parallel_for_item_separate_source_host.cpp
+++ tests/parallel_for_item_separate_source/parallel_for_item_separate_source_host.cpp
@@ -9,18 +9,26 @@ constexpr size_t N = 5;
 
 // For the purposes of linking
 // The OpenCL implementation would load this into the Kernel class behind the scenes
-void myKernel(
+void myKernelTraditionalWorkItemEntryPoint(
     cl::sycl::nd_item<1>,
     cl::sycl::device_queue,
     int,
     int *);
-void myKernel2(
+void myKernelKernelEntryPoint(
     cl::sycl::handler cgh,
     cl::sycl::nd_range<1> range,
     cl::sycl::device_queue dq,
     int addValue,
     int *output
     );
+void myKernelWGEntryPoint(
+  cl::sycl::handler cgh,
+  cl::sycl::range<1> range,
+  item<1> groupIndex,
+  cl::sycl::device_queue dq,
+  int addValue,
+  int *output
+  );
 
 int main() 
 {    
@@ -32,7 +40,7 @@ int main()
 
         Program p;
         
-        auto kernelObject = p.getKernel<decltype(myKernel), cl::sycl::nd_item<1>, cl::sycl::device_queue, int, cl::sycl::accessor<int, 1, cl::sycl::access::write>>(&myKernel);
+        auto kernelObject = p.getKernel<decltype(myKernelTraditionalWorkItemEntryPoint), cl::sycl::nd_item<1>, cl::sycl::device_queue, int, cl::sycl::accessor<int, 1, cl::sycl::access::write>>(&myKernel);
         
         myQueue.submit([&](handler &cgh) {
             auto acc = a.get_access<access::write>(cgh);
@@ -53,7 +61,22 @@ int main()
             auto dq = cgh.get_device_queue();
             // Capture arguments for kernel object
             // cgh.run(nd_range<1>{N, 1}, kernelObject2(dq, 0, acc));
-            myKernel2(cgh, nd_range<1> {N, 1}, dq, 0, acc.get_pointer());
+            myKernelKernelEntryPoint(cgh, nd_range<1> {N, 1}, dq, 0, acc.get_pointer());
+        });
+        VERIFY_BUFFER_VALUE(a, [](id<1> i) { return ((i == 0) ? i[0] : ((i == 1) ? 5 : 6)); });
+
+        // Test interface to show direct kernel call version
+        // We would actually load this through a cl::Kernel as we do for the above one
+        // which would hide away the pointer conversion such that acc could be passed directly
+        // and cgh might or might not be passed... 
+        myQueue.submit([&](handler &cgh) {
+          auto acc = a.get_access<access::write>(cgh);
+          auto dq = cgh.get_device_queue();
+          // Capture arguments for kernel object
+          // cgh.run(nd_range<1>{N, 1}, kernelObject2(dq, 0, acc));
+          cgh.parallel_for(range<1> {N}, [&](item<1> groupIndex) {
+            myKernelWGEntryPoint(cgh, range<1> {N}, groupIndex, dq, 0, acc.get_pointer());
+          });
         });
         VERIFY_BUFFER_VALUE(a, [](id<1> i) { return ((i == 0) ? i[0] : ((i == 1) ? 5 : 6)); });
     }
