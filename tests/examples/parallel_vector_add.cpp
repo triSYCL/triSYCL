e4a4959c582dc3001c1af1c2ce88c5444aa29c23
Ronan Keryell <ronan@keryell.fr>
2015-09-29 15:32:10 +0200

    Simplify parallel vector add example to fit better in a single slide

--- tests/examples/parallel_vector_add.cpp
+++ tests/examples/parallel_vector_add.cpp
@@ -4,8 +4,7 @@
 */
 #include <CL/sycl.hpp>
 #include <iostream>
-
-using namespace cl::sycl;
+#include <iterator>
 
 constexpr size_t N = 3;
 using Vector = float[N];
@@ -13,42 +12,39 @@ using Vector = float[N];
 int main() {
   Vector a = { 1, 2, 3 };
   Vector b = { 5, 6, 8 };
-  float c[N];
+  Vector c;
 
   { // By sticking all the SYCL work in a {} block, we ensure
     // all SYCL tasks must complete before exiting the block
 
     // Create a queue to work on
-    queue myQueue;
+    cl::sycl::queue myQueue;
 
-    // Create buffers from a & b vectors with 2 different syntax
-    buffer<float> A (std::begin(a), std::end(a));
-    buffer<float> B { std::begin(b), std::end(b) };
+    // Create buffers from a & b vectors
+    cl::sycl::buffer<float> A { std::begin(a), std::end(a) };
+    cl::sycl::buffer<float> B { std::begin(b), std::end(b) };
 
     // A buffer of N float using the storage of c
-    buffer<float> C(c, N);
+    cl::sycl::buffer<float> C { c, N };
 
     /* The command group describing all operations needed for the kernel
        execution */
-    myQueue.submit([&](handler &cgh) {
+    myQueue.submit([&](cl::sycl::handler &cgh) {
       // In the kernel A and B are read, but C is written
-      auto ka = A.get_access<access::read>(cgh);
-      auto kb = B.get_access<access::read>(cgh);
-      auto kc = C.get_access<access::write>(cgh);
+      auto ka = A.get_access<cl::sycl::access::read>(cgh);
+      auto kb = B.get_access<cl::sycl::access::read>(cgh);
+      auto kc = C.get_access<cl::sycl::access::write>(cgh);
 
       // Enqueue a parallel kernel
-      cgh.parallel_for<class vector_add>(range<1> { N },
-                                         [=] (id<1> index) {
-                                           std::cout << index.get(0) << " ";
+      cgh.parallel_for<class vector_add>(cl::sycl::range<1> { N },
+                                         [=] (cl::sycl::id<1> index) {
                                            kc[index] = ka[index] + kb[index];
                                          });
       }); // End of our commands for this queue
   } // End scope, so we wait for the queue to complete
 
   std::cout << std::endl << "Result:" << std::endl;
-  for(int i = 0; i != N; i++)
-    std::cout << c[i] << " ";
+  for(auto e : c)
+    std::cout << e << " ";
   std::cout << std::endl;
-
-  return 0;
 }
