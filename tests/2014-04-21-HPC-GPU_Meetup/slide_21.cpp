46ad590b8e9095c05f73eff0d68a2eda42a39afc
Ronan Keryell <ronan.keryell@xilinx.com>
2015-11-11 01:41:55 +0000

    Fix example to avoid 2 kernels in the same command group
    Use 2 submissions to the queue instead.

--- tests/2014-04-21-HPC-GPU_Meetup/slide_21.cpp
+++ tests/2014-04-21-HPC-GPU_Meetup/slide_21.cpp
@@ -8,9 +8,10 @@ int main()
   int result;
   using namespace cl::sycl;
   cl::sycl::queue myQueue;
-  cl::sycl::buffer<int> resultBuf(&result, 1);
-  myQueue.submit([&](handler &cgh) {
-      auto writeResult = resultBuf.get_access<cl::sycl::access::write>(cgh);
+  {
+    cl::sycl::buffer<int> resultBuf(&result, 1);
+    myQueue.submit([&](handler &cgh) {
+        auto writeResult = resultBuf.get_access<cl::sycl::access::write>(cgh);
 //////// Start left side of the slide
 cgh.single_task<class simple_test>([=] ()
             {
@@ -18,14 +19,17 @@ cgh.single_task<class simple_test>([=] ()
             }
     );
 //////// End left side of the slide
+      });
 
+    myQueue.submit([&](handler &cgh) {
 //////// Start right side of the slide
  cgh.parallel_for<class example_kernel>(nd_range<3>(range<3>(4, 40, 4), range<3>(2, 2, 2)),
       [=]  (nd_item<3> t_item) {
         t_item.barrier(access::fence_space::local_space);
       });
 //////// End right side of the slide
-    });
+      });
+  }
   std::cout << "Result = " << result << std::endl;
   return 0;
 }
