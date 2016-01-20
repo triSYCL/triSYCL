fe3965c8fcdcbf2d456077c0f81ef4a8a7bd1a9b
Ronan Keryell <ronan.keryell@xilinx.com>
2016-01-20 14:52:16 +0000

    Update & add examples for HiPEAC tutorial, WRC and AND REFORM workshop
    Fix missing <numeric>.
    Remove useless block.

--- tests/examples/demo_parallel_matrices_add.cpp
+++ tests/examples/demo_parallel_matrices_add.cpp
@@ -10,75 +10,69 @@ const size_t N = 2000;
 const size_t M = 3000;
 
 int main() {
-  { // By sticking all the SYCL work in a {} block, we ensure
-    // all SYCL tasks must complete before exiting the block
+  // Create a queue to work on
+  queue myQueue;
 
-    // Create a queue to work on
-    queue myQueue;
+  // Create some 2D buffers of float for our matrices
+  buffer<double, 2> a({ N, M });
+  buffer<double, 2> b({ N, M });
+  buffer<double, 2> c({ N, M });
 
-    // Create some 2D buffers of float for our matrices
-    buffer<double, 2> a({ N, M });
-    buffer<double, 2> b({ N, M });
-    buffer<double, 2> c({ N, M });
+  // Launch a first asynchronous kernel to initialize a
+  myQueue.submit([&](handler &cgh) {
+      // The kernel write a, so get a write accessor on it
+      auto A = a.get_access<access::write>(cgh);
 
-    // Launch a first asynchronous kernel to initialize a
-    myQueue.submit([&](handler &cgh) {
-        // The kernel write a, so get a write accessor on it
-        auto A = a.get_access<access::write>(cgh);
+      // Enqueue a parallel kernel iterating on a N*M 2D iteration space
+      cgh.parallel_for<class init_a>({ N, M },
+                                     [=] (id<2> index) {
+                                       A[index] = index[0]*2 + index[1];
+                                     });
+    });
 
-        // Enqueue a parallel kernel iterating on a N*M 2D iteration space
-        cgh.parallel_for<class init_a>({ N, M },
-                                       [=] (id<2> index) {
-                                         A[index] = index[0]*2 + index[1];
-                                       });
-      });
+  // Launch an asynchronous kernel to initialize b
+  myQueue.submit([&](handler &cgh) {
+      // The kernel write b, so get a write accessor on it
+      auto B = b.get_access<access::write>(cgh);
+      /* From the access pattern above, the SYCL runtime detect this
+         command_group is independant from the first one and can be
+         scheduled independently */
 
-    // Launch an asynchronous kernel to initialize b
-    myQueue.submit([&](handler &cgh) {
-        // The kernel write b, so get a write accessor on it
-        auto B = b.get_access<access::write>(cgh);
-        /* From the access pattern above, the SYCL runtime detect this
-           command_group is independant from the first one and can be
-           scheduled independently */
+      // Enqueue a parallel kernel iterating on a N*M 2D iteration space
+      cgh.parallel_for<class init_b>({ N, M },
+                                     [=] (id<2> index) {
+                                       B[index] = index[0]*2014 + index[1]*42;
+                                     });
+    });
 
-        // Enqueue a parallel kernel iterating on a N*M 2D iteration space
-        cgh.parallel_for<class init_b>({ N, M },
-                                       [=] (id<2> index) {
-                                         B[index] = index[0]*2014 + index[1]*42;
-                                       });
-      });
+  // Launch an asynchronous kernel to compute matrix addition c = a + b
+  myQueue.submit([&](handler &cgh) {
+      // In the kernel a and b are read, but c is written
+      auto A = a.get_access<access::read>(cgh);
+      auto B = b.get_access<access::read>(cgh);
+      auto C = c.get_access<access::write>(cgh);
+      // From these accessors, the SYCL runtime will ensure that when
+      // this kernel is run, the kernels computing a and b completed
 
-    // Launch an asynchronous kernel to compute matrix addition c = a + b
-    myQueue.submit([&](handler &cgh) {
-        // In the kernel a and b are read, but c is written
-        auto A = a.get_access<access::read>(cgh);
-        auto B = b.get_access<access::read>(cgh);
-        auto C = c.get_access<access::write>(cgh);
-        // From these accessors, the SYCL runtime will ensure that when
-        // this kernel is run, the kernels computing a and b completed
+      // Enqueue a parallel kernel iterating on a N*M 2D iteration space
+      cgh.parallel_for<class matrix_add>({ N, M },
+                                         [=] (id<2> index) {
+                                           C[index] = A[index] + B[index];
+                                         });
+    });
 
-        // Enqueue a parallel kernel iterating on a N*M 2D iteration space
-        cgh.parallel_for<class matrix_add>({ N, M },
-                                           [=] (id<2> index) {
-                                             C[index] = A[index] + B[index];
-                                           });
-      });
-
-    /* Request an access to read c from the host-side. The SYCL runtime
-       ensures that c is ready when the accessor is returned */
-    auto C = c.get_access<access::read, access::host_buffer>();
-    std::cout << std::endl << "Result:" << std::endl;
-    for(size_t i = 0; i < N; i++)
-      for(size_t j = 0; j < M; j++)
-        // Compare the result to the analytic value
-        if (C[i][j] != i*(2 + 2014) + j*(1 + 42)) {
-          std::cout << "Wrong value " << C[i][j] << " on element "
-                    << i << ' ' << j << std::endl;
-          exit(-1);
-        }
-
-  } /* End scope of myQueue, this wait for any remaining operations on the
-       queue to complete */
+  /* Request an access to read c from the host-side. The SYCL runtime
+     ensures that c is ready when the accessor is returned */
+  auto C = c.get_access<access::read, access::host_buffer>();
+  std::cout << std::endl << "Result:" << std::endl;
+  for(size_t i = 0; i < N; i++)
+    for(size_t j = 0; j < M; j++)
+      // Compare the result to the analytic value
+      if (C[i][j] != i*(2 + 2014) + j*(1 + 42)) {
+        std::cout << "Wrong value " << C[i][j] << " on element "
+                  << i << ' ' << j << std::endl;
+        exit(-1);
+      }
 
   std::cout << "Good computation!" << std::endl;
   return 0;
---
+++ tests/examples/simpler_parallel_matrix_add.cpp
@@ -0,0 +1,41 @@
+/* RUN: %{execute}%s | %{filecheck} %s
+   CHECK: c[0][2] = 7
+*/
+#include <CL/sycl.hpp>
+#include <iostream>
+
+using namespace cl::sycl;
+
+constexpr size_t N = 2;
+constexpr size_t M = 3;
+using Matrix = float[N][M];
+
+// Compute sum of matrices a and b into c
+int main() {
+ Matrix a = { { 1, 2, 3 }, { 4, 5, 6 } };
+ Matrix b = { { 2, 3, 4 }, { 5, 6, 7 } };
+
+ Matrix c;
+
+ {// Create a queue to work on default device
+  queue q;
+  // Wrap some buffers around our data
+  buffer<float, 2> A { &a[0][0], range<2> { N, M } };
+  buffer<float, 2> B { &b[0][0], range<2> { N, M } };
+  buffer<float, 2> C { &c[0][0], range<2> { N, M } };
+  // Enqueue some computation kernel task
+  q.submit([&](handler& cgh) {
+   // Define the data used/produced
+   auto ka = A.get_access<access::read>(cgh);
+   auto kb = B.get_access<access::read>(cgh);
+   auto kc = C.get_access<access::write>(cgh);
+   // Create & call kernel named "mat_add"
+   cgh.parallel_for<class mat_add>(range<2> { N, M },
+      [=](id<2> i) { kc[i] = ka[i] + kb[i]; }
+   );
+  }); // End of our commands for this queue
+ } // End scope, so wait for the queue to complete.
+ // Copy back the buffer data with RAII behaviour.
+ std::cout << "c[0][2] = " << c[0][2] << std::endl;
+ return 0;
+}
