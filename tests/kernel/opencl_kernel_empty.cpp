c593d51b104c8fda268a4475e02cdb882e4c9eb3
Ronan Keryell <ronan.keryell@xilinx.com>
2016-07-05 16:38:14 +0100

    Add new OpenCL kernel tests
    Rename a former one too.
    Current state is still a WIP.

--- tests/kernel/opencl_kernel_empty.cpp
+++ tests/kernel/opencl_kernel_empty.cpp
@@ -10,8 +10,6 @@
 
 using namespace cl::sycl;
 
-constexpr size_t N = 30;
-
 int test_main(int argc, char *argv[]) {
   // Construct an OpenCL program from the source string
   auto program = boost::compute::program::create_with_source(R"(
@@ -24,20 +22,6 @@ int test_main(int argc, char *argv[]) {
      so we do not have to have a clRetainKernel */
   auto bk = boost::compute::kernel { program, "empty" };
   kernel k { bk.get() };
-
-  // Verify that the cache works when asking for same kernel
-  kernel k2 { bk.get() };
-  BOOST_CHECK(k == k2);
-
-  /* Do not use the default assignment check check_assignment() since
-     there is no default constructor. Use an explicit check */
-  k2 = k;
-  BOOST_CHECK(k == k2);
-
-  // The other usual checks
-  check_copy(k);
-  check_associative_ordered_container(k);
-  check_associative_unordered_container(k);
 
   // Construct the queue from the defaul OpenCL one
   queue q { boost::compute::system::default_queue() };
