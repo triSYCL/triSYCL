fced0263ca61558717e76d7132a4bbf4e8964f79
Ronan Keryell <ronan.keryell@xilinx.com>
2016-09-02 17:48:22 +0100

    Fix a deadlock when the buffer waiter is waiting for a buffer destructor
    When the buffer waiter destructor is waiting for the buffer destruction
    and it is actually the only owner, there is nobody else to notify for
    the completion...

--- tests/buffer/global_buffer_host_access.cpp
+++ tests/buffer/global_buffer_host_access.cpp
@@ -8,16 +8,14 @@
 #include <CL/sycl.hpp>
 #include <iostream>
 
-#ifdef _OPENMP
-#include <omp.h>
-#endif
-
 using namespace cl::sycl;
 
 constexpr size_t N = 2;
 constexpr size_t M = 3;
 using Matrix = float[N][M];
 
+/* A global buffer. Beware that its destructor is executed after
+   main() exit! */
 buffer<float, 2> A;
 
 int main() {
@@ -26,13 +24,27 @@ int main() {
   // Create buffers from a, b & c storage
   A = { &a[0][0], range<2> { N, M } };
 
-  auto c = A.get_access<access::mode::read>();
+  {
+    /* Limit the scope of the accessor to avoid to keep a reference on
+       the buffer that would prevent its destruction by the
+       assignement later
 
-  std::cout << std::endl << "Result:" << std::endl;
-  for (size_t i = 0; i != N; i++)
-    for (size_t j = 0; j != M; j++)
-      std::cout << c[i][j] << " ";
-  std::cout << std::endl;
+       \todo Should we precise that a host accessor keep a reference
+       on the underlying buffer?
+    */
+    auto c = A.get_access<access::mode::read>();
+
+    std::cout << std::endl << "Result:" << std::endl;
+    for (size_t i = 0; i != N; i++)
+      for (size_t j = 0; j != M; j++)
+        std::cout << c[i][j] << " ";
+    std::cout << std::endl;
+  }
+  /* Since the global buffer destructor is called outside of main but
+     will copy back to Matrix a that will no longer exist, execute the
+     destructor now by assigning an empty buffer to A
+  */
+  A = {};
 
   return 0;
 }
