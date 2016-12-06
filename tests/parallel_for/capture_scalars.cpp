27b853ce4f64e6fcf54c0f4914aa7c33316b428d
Ronan Keryell <ronan.keryell@xilinx.com>
2016-12-06 09:25:16 +0000

    Add test case with scalar capture

---
+++ tests/parallel_for/capture_scalars.cpp
@@ -0,0 +1,34 @@
+/* RUN: %{execute}%s
+
+   Simple parallel for with some external scalars that are captured
+*/
+#include <CL/sycl.hpp>
+#include <iostream>
+#include "test-helpers.hpp"
+
+using namespace cl::sycl;
+
+constexpr size_t N = 30;
+
+int main() {
+  queue q;
+
+  buffer<unsigned int,1> a { N };
+  int off { 123 };
+  float off_float { 1.5F };
+  q.submit([&] (handler &cgh) {
+      auto acc = a.get_access<access::mode::write>(cgh);
+      // Show that we can use a simple parallel_for with int, for example
+      cgh.parallel_for<class nothing>(N, [=] (int index) {
+          acc[index] = index + off*off_float;
+        });
+    });
+  // Verify the computation
+  VERIFY_BUFFER_VALUE(a,
+                      [=](id<1> i) -> decltype(a)::value_type
+                      {
+                        return i[0] + off*off_float;
+                      });
+
+  return 0;
+}
