321e778b3510fb0eec8251fbc2fc2eac87dee6e1
Thibault Joan (Xilinx) <joan.thibault@ens-rennes.fr>
2017-02-07 18:05:07 +0000

    fix bug

--- tests/device/get_info.cpp
+++ tests/device/get_info.cpp
@@ -5,17 +5,17 @@
 #include <boost/test/minimal.hpp>
 
 using namespace cl::sycl;
-using namespace cl::sycl::info::device;
 
 int test_main(int argc, char *argv[]) {
   // Get all the devices
   auto devices = device::get_devices();
   // Verify that the device_type returned match what is expected
   for (auto const &d : devices) {
-    BOOST_CHECK(d.get_info<max_work_group_size>() == 63);
-    BOOST_CHECK(d.get_info<max_compute_units>() == 56);
-    BOOST_CHECK(d.get_info<device_type>() == info::device_type::cpu);
-    BOOST_CHECK(d.get_info<local_mem_size>() == 32000);
+    BOOST_CHECK(d.get_info<info::device::max_work_group_size>() == 63);
+    BOOST_CHECK(d.get_info<info::device::max_compute_units>() == 56);
+    BOOST_CHECK(d.get_info<info::device::device_type>() ==
+        info::device_type::cpu);
+    BOOST_CHECK(d.get_info<info::device::local_mem_size>() == 32000);
   }
 
   return 0;
