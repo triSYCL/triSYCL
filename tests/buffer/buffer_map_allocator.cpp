1f489ce512da616a6cbbdf513320f57f3ca33234
Thibault Joan (Xilinx) <joan.thibault@ens-rennes.fr>
2017-02-13 18:37:16 +0000

    upgrade test case map_allocator with location check

--- tests/buffer/buffer_map_allocator.cpp
+++ tests/buffer/buffer_map_allocator.cpp
@@ -14,14 +14,17 @@ int test_main(int argc, char *argv[]) {
 
   std::vector<int> vect(N, 42);
 
-  {
-    cl::sycl::buffer<int, 1, cl::sycl::map_allocator<int> > dest_buf
-      { vect.data(), cl::sycl::range<1>(N) };
-  }
+  cl::sycl::buffer<int, 1, cl::sycl::map_allocator<int> > buff
+    { vect.data(), cl::sycl::range<1>(N) };
 
+  auto read = buff.get_access
+    <sycl::access::mode::read, sycl::access::target::host_buffer>();
   for (int i = 0; i < N; ++i) {
     // std::cerr << vect[i] << ':' << i << std::endl;
-    BOOST_CHECK(vect[i] == 0);
+    // check that the buffer has been properly 're-initialized'
+    BOOST_CHECK(read[i] == 0);
+    // check the buffer's location
+    BOOST_CHECK(&vect[i] == &read[i]);
   }
 
   return 0;
