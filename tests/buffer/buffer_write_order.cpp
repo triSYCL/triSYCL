5251648c86236888d4c8857c9d04be711d074fcc
Anastasios Doumoulakis <a.doumoulakis@gmail.com>
2017-05-19 13:17:06 +0100

    Fix debug in CMake and Add test for buffer write order

---
+++ tests/buffer/buffer_write_order.cpp
@@ -0,0 +1,23 @@
+#include <CL/sycl.hpp>
+#include "test-helpers.hpp"
+
+using namespace cl::sycl;
+
+constexpr size_t N = 3;
+using Vector = float[N];
+
+int main() {
+  queue q;
+  
+  buffer<int> A(N);
+  
+  q.submit([&] (handler &cgh) {
+      auto ka = A.get_access<access::mode::write>(cgh);
+      auto stub = A.get_access<access::mode::read>(cgh);
+      cgh.parallel_for<>(N, [=] (int index) {
+          ka[index] = index;
+        });
+    });
+  q.wait();
+  return 0;
+}
