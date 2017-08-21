ca1277b1a28afcdb3ac737afd64fdc04c4b7405d
Anastasios Doumoulakis <a.doumoulakis@gmail.com>
2017-08-21 15:05:14 +0100

    Fix platform.get_devices tests

--- tests/platform/get_devices.cpp
+++ tests/platform/get_devices.cpp
@@ -10,27 +10,30 @@
 using namespace cl::sycl;
 
 int test_main(int argc, char *argv[]) {
-  platform p;
-
-  vector_class<device> devices = p.get_devices();
 
 #ifdef TRISYCL_OPENCL
-  // Check if all the devices from boost compute are in the device vector
-  for (const auto &d : boost::compute::system::devices()) {
-    device s_d { d };
-    BOOST_CHECK(std::find(devices.begin(), devices.end(),
-                          s_d) != devices.end());
-  }
-  // Check if the host device is in the device vector
-  device host_dev;
-  BOOST_CHECK(std::find(devices.begin(), devices.end(),
-                        host_dev) != devices.end());
+  // We check that \c get_device in triSYCL returns the same devices
+  // that \c get_device in Boost Compute
+  for (const auto &p : boost::compute::system::platforms()) {
 
-#else
+    platform s_p { p };
+    std::vector<device> devices = s_p.get_devices();
+
+    for(const auto &d : p.devices()) {
+      device s_d { d };
+      BOOST_CHECK(std::find(devices.begin(), devices.end(),
+                            s_d) != devices.end());
+    }
+  }
+#endif
+
+  // We test get_devices for the host platform
+  platform p;
+  vector_class<device> devices = p.get_devices();
+
   // There should be only the host device
   BOOST_CHECK(devices.size() == 1);
   BOOST_CHECK(devices[0].is_host());
-#endif
 
   return 0;
 }
