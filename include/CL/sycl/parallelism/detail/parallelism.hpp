417aed1f1350d854f6e1355ab634432bc3225110
Lin Ya Yu <yu810226@gmail.com>
2017-11-24 12:29:22 +0000

    Modify the header file to change get() to get_id() for group.

--- include/CL/sycl/parallelism/detail/parallelism.hpp
+++ include/CL/sycl/parallelism/detail/parallelism.hpp
@@ -352,7 +352,7 @@ void parallel_for_workitem(const group<Dimensions> &g,
     // Reconstruct the global nd_item
     index.set_local(local);
     // \todo Some strength reduction here
-    index.set_global(local + id<Dimensions>(g.get_local_range())*g.get());
+    index.set_global(local + id<Dimensions>(g.get_local_range())*g.get_id());
     // Call the user kernel at last
     f(index);
   };
