43be764f5438662ab87fba7936d88e5723fd3bf6
Lin Ya Yu <yu810226@gmail.com>
2017-12-15 17:03:06 +0000

    Add decorating functions for Xilinx tools supported optimization: dataflow, pipeline

---
+++ include/CL/sycl/vendor/Xilinx/opt_decorate_func.hpp
@@ -0,0 +1,56 @@
+#ifndef TRISYCL_SYCL_VENDOR_DECORATE_FUNC_HPP
+#define TRISYCL_SYCL_VENDOR_DECORATE_FUNC_HPP
+
+/** \file This file includes some decorating functions for Xilinx tools
+    supported optimization.
+
+    This file is distributed under the University of Illinois Open Source
+    License. See LICENSE.TXT for details.
+*/
+
+namespace cl {
+namespace sycl {
+namespace vendor {
+
+
+/** Applying dataflow on functions
+
+    dataflow function taking functions that user wants to apply dataflow on as
+    arguments.
+*/
+template <typename Functor>
+void dataflow(Functor f) noexcept {
+  /* ssdm instruction is inserted before the argument functor to guide xocc to
+     do dataflow. */
+  _ssdm_op_SpecDataflowPipeline(-1, "");
+  f();
+}
+
+
+/** Applying pipeline on loops
+
+    pipeline function taking  that user wants to
+    apply pipeline on as arguments.
+*/
+template <typename Functor>
+void pipeline(Functor f) noexcept {
+  /* ssdm instruction is inserted before the argument functor to guide xocc to
+     do pipeline. */
+  _ssdm_op_SpecPipeline(-1, 1, 1, 0, "");
+  f();
+}
+
+
+}
+}
+}
+
+/*
+    # Some Emacs stuff:
+    ### Local Variables:
+    ### ispell-local-dictionary: "american"
+    ### eval: (flyspell-prog-mode)
+    ### End:
+*/
+
+#endif // TRISYCL_SYCL_VENDOR_DECORATE_FUNC_HPP
