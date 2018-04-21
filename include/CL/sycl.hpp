e68c724fff1fb6c26446168c5de3c87d9180d6fe
Ronan Keryell <ronan.keryell@xilinx.com>
2018-04-20 19:38:48 -0700

    Update copyright date to 2018

--- include/CL/sycl.hpp
+++ include/CL/sycl.hpp
@@ -20,7 +20,7 @@
 
     Copyright 2014--2015 Advanced Micro Devices, Inc.
 
-    Copyright 2015--2017 Xilinx, Inc.
+    Copyright 2015--2018 Xilinx, Inc.
 
     This file is distributed under the University of Illinois Open Source
     License. See LICENSE.TXT for details.
