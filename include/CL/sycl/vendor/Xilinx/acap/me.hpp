aa3f962c35b770064cd354617d240ce058ddd445
Ronan Keryell <ronan.keryell@xilinx.com>
2018-06-14 14:16:23 -0700

    Add forgotten master include file for MathEngine Xilinx vendor extension

---
+++ include/CL/sycl/vendor/Xilinx/acap/me.hpp
@@ -0,0 +1,25 @@
+#ifndef TRISYCL_SYCL_VENDOR_XILINX_ACAP_ME_HPP
+#define TRISYCL_SYCL_VENDOR_XILINX_ACAP_ME_HPP
+
+/** \file
+
+    Experimenting with processor arrays such as ACAP/MathEngine
+
+    2017-09-18--22-Khronos_F2F_Chicago-Xilinx/2017-09-19--20-Xilinx-SYCL-Next.pdf
+    Based on Math Engine (ME) Architecture Specification, Revision v1.4
+    March 2018
+*/
+
+#include "me/array.hpp"
+#include "me/geography.hpp"
+#include "me/tile.hpp"
+
+/*
+    # Some Emacs stuff:
+    ### Local Variables:
+    ### ispell-local-dictionary: "american"
+    ### eval: (flyspell-prog-mode)
+    ### End:
+*/
+
+#endif // TRISYCL_SYCL_VENDOR_XILINX_ACAP_ME_HPP
