6d1ee235a0f024934fee658c705c3a03c8a58c67
Ronan Keryell <ronan.keryell@xilinx.com>
2018-07-16 22:08:53 +0200

    Update cascade stream ME example to new memory module concept

--- tests/acap/cascade_stream.cpp
+++ tests/acap/cascade_stream.cpp
@@ -10,27 +10,26 @@
 using namespace cl::sycl;
 using namespace cl::sycl::vendor::xilinx;
 
+
 /// A small MathEngine program
-template <typename Geography, typename ME_Array, int X, int Y>
-struct tile
-  : acap::me::tile<Geography, ME_Array, X, Y> {
-  using t = acap::me::tile<Geography, ME_Array, X, Y>;
-  using geo = Geography;
+template <typename ME_Array, int X, int Y>
+struct tile : acap::me::tile<ME_Array, X, Y> {
+  using t = acap::me::tile<ME_Array, X, Y>;
 
   void run(ME_Array &a) {
     std::cout << "Hello, I am the ME tile (" << X << ',' << Y
               << ") using " << sizeof(*this) << " bytes of memory "
               << std::endl;
     // Do not read at the start of the cascade
-    if constexpr (!(t::x == geo::x_min && t::y == geo::y_min)) {
+    if constexpr (!(t::x == t::geo::x_min && t::y == t::geo::y_min)) {
         auto cs_in = this->template get_cascade_stream_in<int>()
           .template get_access<access::mode::read,
                                access::target::blocking_pipe>();
        std::cout << "Reading " << cs_in.read() << std::endl;
     }
     // Do not write at the end of the cascade
-    if constexpr (!(t::x == ((t::y & 1) ? geo::x_min : geo::x_max)
-                    && t::y == geo::y_max)) {
+    if constexpr (!(t::x == ((t::y & 1) ? t::geo::x_min : t::geo::x_max)
+                    && t::y == t::geo::y_max)) {
         auto cs_out = t::template get_cascade_stream_out<int>()
           .template get_access<access::mode::write,
                                access::target::blocking_pipe>();
@@ -43,7 +42,8 @@ struct tile
 int main() {
   std::cout << std::endl << "Instantiate big MathEngine:"
             << std::endl << std::endl;
-  // acap::me::array<me_layout_1pe, tile> me;
-  acap::me::array<acap::me::layout::full, tile> me;
+
+  // Use directly acap::me::memory as an empty memory module
+  acap::me::array<acap::me::layout::full, tile, acap::me::memory> me;
   me.run();
 }
