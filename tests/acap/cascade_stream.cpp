f36ee9ea1791cd936e3f9ef50c9012e18fbc05f5
Ronan Keryell <ronan.keryell@xilinx.com>
2018-08-17 10:49:45 -0700

    Clean up cascade stream for slide inclusion

--- tests/acap/cascade_stream.cpp
+++ tests/acap/cascade_stream.cpp
@@ -13,33 +13,30 @@ using namespace cl::sycl::vendor::xilinx;
 
 /// A small MathEngine program
 template <typename ME_Array, int X, int Y>
-struct tile : acap::me::tile<ME_Array, X, Y> {
+struct tile_program : acap::me::tile<ME_Array, X, Y> {
   using t = acap::me::tile<ME_Array, X, Y>;
 
   void run() {
     // Do not read at the start of the cascade
     if constexpr (!t::is_cascade_start()) {
-        auto cs_in = this->template get_cascade_stream_in<int>();
-        auto v = cs_in.read();
-        std::cout << "< Tile(" << X << ',' << Y << ") is reading "
-                  << v << std::endl;
+      auto cs_in = this->template get_cascade_stream_in<int>();
+      auto v = cs_in.read();
+      std::cout << "< Tile(" << X << ',' << Y << ") is reading "
+                << v << std::endl;
     }
     // Do not write at the end of the cascade
     if constexpr (!t::is_cascade_end()) {
-        auto cs_out = t::template get_cascade_stream_out<int>();
-        auto v = t::x*1000 + t::y;
-        std::cout << "> Tile(" << X << ',' << Y << ") is writing "
-                  << v << std::endl;
-        cs_out << v;
+      auto cs_out = t::template get_cascade_stream_out<int>();
+      auto v = t::x*1000 + t::y;
+      std::cout << "> Tile(" << X << ',' << Y << ") is writing "
+                << v << std::endl;
+      cs_out << v;
     }
   }
 };
 
 
 int main() {
-  std::cout << std::endl << "Instantiate big MathEngine:"
-            << std::endl << std::endl;
-
   // Use an empty memory module
-  acap::me::array<acap::me::layout::full, tile>{}.run();
+  acap::me::array<acap::me::layout::full, tile_program>{}.run();
 }
