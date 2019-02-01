c98649dac9fd7aaf423d762bf3fbd11ef0d30537
Ronan Keryell <ronan.keryell@xilinx.com>
2019-01-31 17:51:59 -0800

    Create acap and acap_debug Doxygen groups

--- include/CL/sycl/vendor/Xilinx/acap/debug.hpp
+++ include/CL/sycl/vendor/Xilinx/acap/debug.hpp
@@ -13,10 +13,28 @@
 
 #include <experimental/mdspan>
 
+/** \defgroup acap Extensions for Xilinx ACAP Versal
+
+    Some classes and functions to support devices including CPU, FPGA
+    & AI Engine CGRA
+    @{
+*/
+
+/** \defgroup acap_debug Debugging support for ACAP
+
+    Some classes and functions to help debuging ACAP applications,
+    such as verifying BSP forward progress
+    @{
+*/
+
 namespace cl::sycl::vendor::xilinx::acap::debug {
 
 /** Verify that an SPMD program has a bulk-synchronous behaviour
- */
+    across all the tiles of the CGRA.
+
+    If there are some discrepancies, display the tile id which is too
+    late or too early.
+*/
 template <typename Geography>
 class bsp_checker {
   /// A vector clock to compare differerent tiles together
@@ -29,7 +47,11 @@ class bsp_checker {
 public:
 
   /** Check that none of the tiles executing this are separated by
-      more than one execution step eachother */
+      more than one execution step each other.
+
+      This functions has to be called in a BSP synchronous way by all
+      the tiles.
+  */
   void check(int x, int y) {
     ++vector_clock(x, y);
     /* While the algorithm execution is not atomic, it is enough to
@@ -49,6 +71,10 @@ public:
 };
 
 }
+
+/// @} End the acap_debug Doxygen group
+
+/// @} End the acap Doxygen group
 
 /*
     # Some Emacs stuff:
