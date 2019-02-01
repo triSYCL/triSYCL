fcb89b5ee8ae88d30ea591c39c8088848f3a7449
Ronan Keryell <ronan.keryell@xilinx.com>
2019-02-01 11:52:14 -0800

    Add Doxygen comments to ACAP AIE geography

--- include/CL/sycl/vendor/Xilinx/acap/aie/geography.hpp
+++ include/CL/sycl/vendor/Xilinx/acap/aie/geography.hpp
@@ -1,7 +1,9 @@
 #ifndef TRISYCL_SYCL_VENDOR_XILINX_ACAP_AIE_GEOGRAPHY_HPP
 #define TRISYCL_SYCL_VENDOR_XILINX_ACAP_AIE_GEOGRAPHY_HPP
 
-/** \file Some geographic information about the AI Engine array
+/** \file
+
+    Geographic information about the AI Engine CGRA
 
     Ronan dot Keryell at Xilinx dot com
 
@@ -13,32 +15,60 @@
 
 namespace cl::sycl::vendor::xilinx::acap::aie {
 
+/// \ingroup aie
+/// @{
+
 /** Some geographic information about the AI Engine array
- */
+
+    \param Layout is a class describing the basic layout of the CGRA,
+    setting for example the x & y size
+*/
 template <typename Layout>
 struct geography : Layout {
+  /// Keep the layout used for this CGRA
   using layout = Layout;
 
+  /// Minimum horizontal coordinate
   static auto constexpr x_min = 0;
+
+  /// Minimum vertical coordinate
   static auto constexpr y_min = 0;
+
+  /// Number of tile horizontally in the CGRA
   static auto constexpr x_size = layout::x_max + 1;
+
+  /// Number of tile vertically in the CGRA
   static auto constexpr y_size = layout::y_max + 1;
 
-  /// Total number of tiles
+  /// Total number of tiles in the CGRA
   static auto constexpr size = x_size*y_size;
 
+
+  /** Compute the linearized coordinate of a tile
+
+      \param[in] x is the horizontal tile coordinate
+
+      \param[in] y is the vertical tile coordinate
+  */
   static auto constexpr linear_id(int x, int y) {
     return x + x_size*y;
   }
 
-  /// A tuple with the coordinate tuples of all the tiles
+  /** A tuple with the coordinate tuples of all the tiles inside the
+      CGRA geometry */
   static auto constexpr tile_coordinates = boost::hana::cartesian_product(
     boost::hana::make_tuple(
         boost::hana::range_c<int, y_min, y_size>
       , boost::hana::range_c<int, x_min, x_size>
                             )
                                                                           );
-  /// Generate a tuple of tileable objects
+
+
+  /** Generate a tuple by tiling the Tileable object onto the CGRA geometry
+
+     \param Tileable is the type to tile according to coordinate \p X
+     and \p Y
+  */
   template <template <int X, int Y> typename Tileable>
   static auto generate_tiles() {
     return boost::hana::transform(
@@ -50,51 +80,78 @@ struct geography : Layout {
                                   );
   }
 
+
+  /** Test if an horizontal coordinate is valid
+
+      \param[in] x is the horizontal tile coordinate
+  */
   static bool constexpr is_x_valid(int x) {
     return x_min <= x && x <= layout::x_max;
   }
 
+
+  /** Test if a vertical coordinate is valid
+
+      \param[in] y is the vertical tile coordinate
+  */
   static bool constexpr is_y_valid(int y) {
     return y_min <= y && y <= layout::y_max;
   }
 
 
-  /// Test if a tile is in the left column
+  /** Test if a tile is in the left column
+
+      \param[in] x is the horizontal tile coordinate
+  */
   static bool constexpr is_left_column(int x) {
     return x == x_min;
   }
 
 
-  /// Test if a tile is in the right column
+  /** Test if a tile is in the right column
+
+      \param[in] x is the horizontal tile coordinate
+  */
   static bool constexpr is_right_column(int x) {
     return x == layout::x_max;
   }
 
 
-  /// Test if a tile is in the bottom row
+  /** Test if a tile is in the bottom row
+
+      \param[in] y is the vertical tile coordinate
+  */
   static bool constexpr is_bottom_row(int y) {
     return y == y_min;
   }
 
 
-  /// Test if a tile is in the top row
+  /** Test if a tile is in the top row
+
+      \param[in] y is the vertical tile coordinate
+  */
   static bool constexpr is_top_row(int y) {
     return y == layout::y_max;
   }
 
-  static bool constexpr is_shim_tile(int x, int y) {
-    // It could be more optimized, but like that it is clearer
-    return layout::is_noc_tile(x, y) || layout::is_pl_tile(x, y);
-  }
 
+  /** Test if a tile owns the start of the cascade_stream
 
-  /// Test if a tile owns the start of the cascade_stream
+      \param[in] x is the horizontal tile coordinate
+
+      \param[in] y is the vertical tile coordinate
+  */
   static bool constexpr is_cascade_start(int x, int y) {
     return x == x_min && y == y_min;
   }
 
 
-  /// Test if a tile owns the end of the cascade_stream
+  /** Test if a tile owns the end of the cascade_stream
+
+      \param[in] x is the horizontal tile coordinate
+
+      \param[in] y is the vertical tile coordinate
+  */
   static bool constexpr is_cascade_end(int x, int y) {
     // The x position depends on the parity of the last line
     return x == ((y & 1) ? x_min : layout::x_max) && y == layout::y_max;
@@ -103,7 +160,11 @@ struct geography : Layout {
 
   /** Test if a memory module coordinate offset is plausible
 
-      1 of the coordinate is 0 and the other one is -1 or 1
+      1 of the displacement is 0 and the other one is -1 or 1
+
+      \param[in] dx is the horizontal displacement (-1,0,+1)
+
+      \param[in] dy is the vertical displacement (-1,0,+1)
   */
   static bool constexpr is_valid_memory_module_offset(int dx, int dy) {
     if (dx == 0)
@@ -115,7 +176,16 @@ struct geography : Layout {
   }
 
 
-  /// Test if a memory module exists and is connected to a tile
+  /** Test if a memory module exists and is connected to a tile
+
+      \param[in] x is the horizontal tile coordinate
+
+      \param[in] y is the vertical tile coordinate
+
+      \param[in] dx is the horizontal displacement (-1,0,+1)
+
+      \param[in] dy is the vertical displacement (-1,0,+1)
+  */
   static bool constexpr is_memory_module(int x, int y, int dx, int dy) {
     if (!is_valid_memory_module_offset(dx, dy))
       return false;
@@ -135,14 +205,28 @@ struct geography : Layout {
   }
 
 
-  /// Compute memory module x coordinate delta from its tile
+  /** Compute the memory module x coordinate delta from its tile
+
+      \param[in] y is the vertical tile coordinate
+
+      \param[in] dx is the horizontal displacement (-1,0,+1)
+  */
   static auto constexpr memory_module_dx_coordinate(int y, int dx) {
     // Mimic Figure 2-20 on p. 65
     return (dx - 1)/2 + (y & 1);
   }
 
 
-  /// Compute the linear id of a memory module attached to a tile
+  /** Compute the linear id of a memory module attached to a tile
+
+      \param[in] x is the horizontal tile coordinate
+
+      \param[in] y is the vertical tile coordinate
+
+      \param[in] dx is the horizontal displacement (-1,0,+1)
+
+      \param[in] dy is the vertical displacement (-1,0,+1)
+  */
   static auto constexpr memory_module_linear_id(int x, int y, int dx, int dy) {
     if (dx == 0)
       return linear_id(x, y + dy);
@@ -151,6 +235,8 @@ struct geography : Layout {
       return linear_id(x + memory_module_dx_coordinate(y, dx), y);
   }
 };
+
+/// @} End the aie Doxygen group
 
 }
 
