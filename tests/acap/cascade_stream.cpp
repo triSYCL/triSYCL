85185d8a7ee8cf3f0c4b7eba1cc3ab11e74a5928
Ronan Keryell <ronan.keryell@xilinx.com>
2019-02-08 19:28:02 -0800

    Comment cascade example

--- tests/acap/cascade_stream.cpp
+++ tests/acap/cascade_stream.cpp
@@ -14,19 +14,22 @@ using namespace cl::sycl::vendor::xilinx;
 /// A small MathEngine program
 template <typename AIE, int X, int Y>
 struct tile_program : acap::aie::tile<AIE, X, Y> {
+  // Get tile information through this shortcut
   using t = acap::aie::tile<AIE, X, Y>;
 
   void run() {
     // Do not read at the start of the cascade
     if constexpr (!t::is_cascade_start()) {
-      auto cs_in = this->template get_cascade_stream_in<int>();
+      // Need template here because of... pure C++
+      auto cs_in = t::template get_cascade_stream_in<int>();
       auto v = cs_in.read();
       std::cout << "< Tile(" << X << ',' << Y << ") is reading "
                 << v << std::endl;
     }
     // Do not write at the end of the cascade
     if constexpr (!t::is_cascade_end()) {
-      auto cs_out = t::template get_cascade_stream_out<int>();
+      // Need template here because of... pure C++. Can use also this->
+      auto cs_out = this->template get_cascade_stream_out<int>();
       auto v = t::x*1000 + t::y;
       std::cout << "> Tile(" << X << ',' << Y << ") is writing "
                 << v << std::endl;
