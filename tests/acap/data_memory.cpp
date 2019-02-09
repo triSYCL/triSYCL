30559d0313d1b700f28d0b7242fd075196a0d847
Ronan Keryell <ronan.keryell@xilinx.com>
2019-02-08 19:29:10 -0800

    Clean up data memory

--- tests/acap/data_memory.cpp
+++ tests/acap/data_memory.cpp
@@ -28,6 +28,7 @@ struct black {
   int i = 7;
 };
 
+/// A memory tile has to inherit from acap::aie::memory<AIE, X, Y>
 template <typename AIE, int X, int Y>
 struct memory
   : acap::aie::memory<AIE, X, Y>
@@ -47,46 +48,13 @@ struct tile : acap::aie::tile<AIE, X, Y> {
   void run() {
    auto &own = t::mem();
 
-   std::cout << "Hello, I am the AI tile (" << X << ',' << Y
-              << ") using " << sizeof(*this) << " bytes of memory "
-              << std::endl;
-    std::cout << "Local v = " << own.v << std::endl;
-    if constexpr (std::remove_reference_t<decltype(own)>::is_white())
-      std::cout << " d = " << own.d << std::endl;
+   std::cout << "Hello, I am the AI tile (" << X << ',' << Y<< ")"
+             << std::endl;
+   std::cout << "Local v = " << own.v << std::endl;
+   if constexpr (std::remove_reference_t<decltype(own)>::is_white())
+                  std::cout << " d = " << own.d << std::endl;
     else
       std::cout << " i = " << own.i << std::endl;
-
-    // auto neighbour_v = get_tile<get_id<0>() ^ 1, get_id<1>()>().v;
-    //std::cout << "Neighbour v = " << a.t1.v << std::endl;
-   // auto neighbour_v = a.template get_tile<2,3>();
-  }
-};
-
-
-
-template <int X>
-struct m {
-  int data;
-};
-
-
-struct me {
-  m<0> mem_0;
-  m<1> mem_1;
-  m<2> mem_2;
-};
-
-template <int X>
-struct t {
-  m<X> mem_0;
-  m<X+1> mem_1;
-
-  template <int XX>
-  auto &get_mem() {
-    if constexpr (XX == 0)
-      return mem_0;
-    else
-      return mem_1;
   }
 };
 
@@ -94,6 +62,7 @@ struct t {
 int main() {
   std::cout << std::endl << "Instantiate small MathEngine:"
             << std::endl << std::endl;
+  // memory is type defining the memory tiles
   acap::aie::array<acap::aie::layout::small, tile, memory> aie;
   aie.run();
 
@@ -101,36 +70,4 @@ int main() {
             << std::endl << std::endl;
   acap::aie::array<acap::aie::layout::one_pe, tile, memory> solitaire_aie;
   solitaire_aie.run();
-/*
-  t tile;
-  std::cout << "0: " << tile.get_mem<0>().data << std::endl;
-  std::cout << "1: " << tile.get_mem<1>().data << std::endl;
-*/
-
-#if 0
-  auto static t = boost::hana::make_tuple(1,2,3);
-  auto static a=  boost::hana::find_if(
-        t
-      , [] (auto tile) {
-          return true;// tile.x == x && tile.y == y;
-        }
-                                );
-
-
-    void compute() {
-      if constexpr (!(is_shim() || is_noc())) {
-        for(;;) {
-          if constexpr (is_red(x,y)) {
-            std::lock_guard l { local_mem<x + 1>::lock(1) };
-            v += 1.5;
-          }
-          else {
-            std::lock_guard l { local_mem<x + 1, y>::lock(2) };
-            i++;
-          }
-        }
-      }
-    }
-  }
-#endif
 }
