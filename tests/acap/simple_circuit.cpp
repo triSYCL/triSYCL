6108a866a5333aa057499e5f7f143b96fe4ffe2e
Ronan Keryell <ronan.keryell@xilinx.com>
2019-03-18 19:26:45 -0700

    Make simple_circuit example more complex with connections from the AIE

--- tests/acap/simple_circuit.cpp
+++ tests/acap/simple_circuit.cpp
@@ -17,19 +17,22 @@ using namespace cl::sycl::vendor::xilinx::acap::aie;
 constexpr auto size = 10;
 
 /// A small AI Engine program
-
 template <typename AIE, int X, int Y>
 struct comm : acap::aie::tile<AIE, X, Y> {
   using t = acap::aie::tile<AIE, X, Y>;
   void run() {
     for (int i = 0; i < size; ++i) {
+      // tile(1,2) read from port 0 and write to port 0
       if constexpr (X == 1 && Y == 2) {
         auto v = t::template in<char>(0).read();
+        // Output = input*2
         t::template out<int>(0) << 2*v;
       }
-      if constexpr (X == 2 && Y == 3) {
+      // tile(2,0) read from port 1 and write to port 0
+      else if constexpr (X == 2 && Y == 0) {
         auto v = t::template in<int>(1).read();
-        t::template out<float>(1) << v + 1.5;
+        // Output = input + 1.5
+        t::template out<float>(0) << v + 1.5;
       }
     }
   }
@@ -40,6 +43,7 @@ int test_main(int argc, char *argv[]) {
   try {
   acap::aie::array<acap::aie::layout::size<3,4>, comm> aie;
 /*
+  Some syntax ideas...
   aie.connect<int>({ 1, 2 }, { 2, 3 });
   aie.connect<float>({ 2, 3 }, shim { 1 });
   aie.connect<float>(all, all);
@@ -58,29 +62,34 @@ int test_main(int argc, char *argv[]) {
 
   // Use the connection from the CPU directly by using the AXI MM to the tile
   aie.tile(1, 2).out<int>(0) << 3;
-  std::cout << aie.tile(2, 0).in<int>(1).read() << std::endl;
+  // Check we read the correct value on tile(2,0) port 1
+  BOOST_CHECK(aie.tile(2, 0).in<int>(1).read() == 3);
 
   // Try a shim & tile connection directly from the host
   aie.shim(0).out<char>(1) << 42;
-  std::cout << int { aie.tile(1, 2).in<char>(0).read() } << std::endl;
+  // Check we read the correct value on tile(1,2) port 0
+  BOOST_CHECK(aie.tile(1, 2).in<char>(0).read() == 42);
 
-exit(1);
   // Launch the AIE
   auto acap = std::thread([&] { aie.run(); });
   // Launch a CPU producer
   std::thread producer { [&] {
       for (int i = 0; i < size; ++i)
         // Use the AXI-MM access to the shim registers to send a value
-        // into the AXI0S from the host
-        /*aie.shim(2).in<char>(0) << i*/;
+        // into the AXI-SS from the host
+        aie.shim(0).out<char>(1) << i;
     }};
   // Launch a CPU consumer
   std::thread consumer { [&] {
-      for (int i = 0; i < size; ++i)
-        /*aie.shim(1).in<float>(1) >> i*/;
+      float f;
+      for (int i = 0; i < size; ++i) {
+        aie.shim(1).in<float>(0) >> f;
+        // Check we read the correct value
+        BOOST_CHECK(f == 2*i + 1.5);
+      }
     }};
 
-  // Waut for everybody to finish
+  // Wait for everybody to finish
   acap.join();
   producer.join();
   consumer.join();
