fa206a1d9d54e23229b2e3b2b85bdd5792cb3aaa
Ronan Keryell <ronan.keryell@xilinx.com>
2019-09-18 17:02:44 -0700

    Update lock_memory example to new API

--- tests/acap/lock_memory.cpp
+++ tests/acap/lock_memory.cpp
@@ -35,12 +35,12 @@ struct tile<AIE, 0, 0> : acap::aie::tile<AIE, 0, 0> {
     auto &m = t::mem_right();
     m.v = 42;
     for (int i = 0; i < 100; ++i) {
-      m.lu.locks[0].acquire_with_value(false);
+      m.lock(0).acquire_with_value(false);
       ++m.v;
       a.update_tile_data_image(t::x, t::y, &m.v, 42, 143);
       std::cout << std::endl << "Tile (0,0) sends to right neighbour: "
                 << m.v << std::endl;
-      m.lu.locks[0].release_with_value(true);
+      m.lock(0).release_with_value(true);
       std::this_thread::sleep_for(20ms);
     }
   }
@@ -54,11 +54,11 @@ struct tile<AIE, 1, 0> : acap::aie::tile<AIE, 1, 0> {
   void run() {
     auto &m = t::mem_left();
     for (int i = 0; i < 100; ++i) {
-      m.lu.locks[0].acquire_with_value(true);
+      m.lock(0).acquire_with_value(true);
       a.update_tile_data_image(t::x, t::y, &m.v, 42, 142);
       std::cout << "Tile (1,0) receives from left neighbour: "
                 << m.v << std::endl;
-      m.lu.locks[0].release_with_value(false);
+      m.lock(0).release_with_value(false);
       std::this_thread::sleep_for(20ms);
     }
   }
@@ -67,11 +67,11 @@ struct tile<AIE, 1, 0> : acap::aie::tile<AIE, 1, 0> {
 int main(int argc, char *argv[]) {
   std::cout << std::endl << "Instantiate small AI Engine:"
             << std::endl << std::endl;
-  acap::aie::array<acap::aie::layout::small, tile, memory> aie;
+  acap::aie::device<acap::aie::layout::small> aie;
 
   a.start(argc, argv, decltype(aie)::geo::x_size,
           decltype(aie)::geo::y_size, 1, 1, 100);
 
   // Launch the AI Engine program
-  aie.run();
+  aie.run<tile, memory>();
 }
