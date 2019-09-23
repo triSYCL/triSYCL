388666f3da7a1483e1a160a4d2f5b384999497c9
Ronan Keryell <ronan.keryell@xilinx.com>
2019-09-23 12:37:58 -0700

    Add empty_program based on AIE program

--- tests/acap/empty_program.cpp
+++ tests/acap/empty_program.cpp
@@ -10,11 +10,15 @@
 using namespace sycl::vendor::xilinx;
 
 int test_main(int argc, char *argv[]) {
-  /* Test the empty tile program & empty memory module for various
+  /* Test the empty tile program & empty memory module with 4
      machine sizes & API shortcut */
   acap::aie::device<acap::aie::layout::one_pe>{}.queue().submit<>().wait();
   acap::aie::device<acap::aie::layout::small>{}.queue().run<>();
   acap::aie::device<acap::aie::layout::full>{}.run<>();
+
+  acap::aie::device<acap::aie::layout::vc1902> d;
+  acap::aie::program p { d };
+  p.run();
 
   return 0;
 }
