11a97838ee9dc70901b29a71fbbdbf62efe40b99
Ronan Keryell <rkeryell@xilinx.com>
2020-04-01 13:37:46 -0700

    Generalize the "AIE NoC to the right" benchmark to many tiles

--- tests/acap/aie_noc_benchmark.cpp
+++ tests/acap/aie_noc_benchmark.cpp
@@ -23,15 +23,15 @@ constexpr auto size = transfers*sizeof(std::int32_t);
 
 // Test neighbor communication from tile(0,0) to tile(1,0)
 template <typename AIE, int X, int Y>
-struct neighbor : acap::aie::tile<AIE, X, Y> {
+struct right_neighbor : acap::aie::tile<AIE, X, Y> {
   using t = acap::aie::tile<AIE, X, Y>;
   void run() {
     for (int i = 0; i < size; ++i) {
-      if constexpr (X == 0 && Y == 0)
-        // tile(0,0) write to port 1
+      if constexpr (!t::is_right_column())
+        // There is a neighbor on the right: send some data
         t::out(1) << i;
-      else if constexpr (X == 1 && Y == 0) {
-        // tile(1,0) read from port 0
+      if constexpr (!t::is_left_column()) {
+        // There is a neighbor on the left: receive some data
         int receive;
         t::in(0) >> receive;
       }
@@ -53,12 +53,29 @@ auto measure_bandwidth = [] (auto transfered_bytes, auto some_work) {
 
 int test_main(int argc, char *argv[]) {
   try {
+    {
+      using d_t = acap::aie::device<acap::aie::layout::size<4,4>>;
+      d_t d;
+      // Test neighbor core connection.
+      // Configure the AIE NoC connections
+      d.for_each_tile_index([&] (auto x, auto y) {
+        // When it is possble, connect each tile to its right neighbor
+        if (d_t::geo::is_x_y_valid(x + 1, y)) {
+          d.tile(x, y).connect(d_t::csp::me_1, d_t::cmp::east_0);
+          d.tile(x + 1, y).connect(d_t::csp::west_0, d_t::cmp::me_0);
+        }
+      });
+      // Only x_max since the last column has no right neigbors to talk to
+      auto transmitted_bytes =
+        sizeof(std::int32_t)*transfers*d_t::geo::x_max*d_t::geo::y_size;
+      measure_bandwidth(transmitted_bytes, [&] { d.run<right_neighbor>(); });
+    }
     using d_t = acap::aie::device<acap::aie::layout::size<2,1>>;
     d_t d;
     // Test neighbor core connection
     d.tile(0,0).connect(d_t::csp::me_1, d_t::cmp::east_0);
     d.tile(1,0).connect(d_t::csp::west_0, d_t::cmp::me_0);
-    measure_bandwidth(size, [&] { d.run<neighbor>(); });
+    measure_bandwidth(size, [&] { d.run<right_neighbor>(); });
   } catch (sycl::exception &e) {
     // Display the string message of any SYCL exception
     std::cerr << e.what() << std::endl;
