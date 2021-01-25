4a6523b9fd1206067fbd18f5eb9d098c235f09b0
Ronan Keryell <rkeryell@xilinx.com>
2021-01-25 21:15:30 +0100

    Add default initialization to xorshift
    Add also unit tests.

--- include/triSYCL/vendor/trisycl/random/xorshift.hpp
+++ include/triSYCL/vendor/trisycl/random/xorshift.hpp
@@ -12,6 +12,7 @@
     License. See LICENSE.TXT for details.
 */
 
+#include <array>
 #include <cstdint>
 #include <type_traits>
 
@@ -33,26 +34,50 @@ struct xorshift {
   /// Bit size of the random generator data type
   static auto constexpr bit_size = Nbits;
 
-  /// The tuple representing the state for xorshift128
-  struct state_128_t {
-    std::uint32_t x, y, z, w;
-  };
+  /// The default initial internal state of the pseudo random generator
+  static auto constexpr initial_state = [] {
+    if constexpr (bit_size == 32) {
+      /* Default value for xor32 from Marsaglia,
+         p. 4 Section "3 Application to Xorshift RNGs" */
+      return std::uint32_t { 2463534242 };
+    } else if constexpr (bit_size == 64) {
+      /* Default value for xor32 from Marsaglia,
+         p. 4 Section "3 Application to Xorshift RNGs" */
+      return std::uint64_t { 88172645463325252 };
+    } else if constexpr (bit_size == 128) {
+        /// Default value for xor128 from Marsaglia, p. 5 Section "4 Summary"
+      return std::array<std::uint32_t, 4>
+        { 123456789, 362436069, 521288629, 88675123 };
+    } else
+      return nullptr;
+  }();
 
   /// The type of the internal state of the generator
-  using state_t = std::conditional_t
-    <bit_size == 32, std::uint32_t, std::conditional_t
-     <bit_size == 64, std::uint64_t, std::conditional_t
-      <bit_size == 128, state_128_t, void>>>;
+  using value_type = std::remove_const_t<decltype(initial_state)>;
 
-  static_assert(!std::is_void_v<state_t>, "Bit size not implemented");
+  static_assert(!std::is_same_v<value_type, std::nullptr_t>,
+                "Bit size not implemented");
 
   /// The internal state of the pseudo random generator
-  state_t state;
+  value_type state = initial_state;
+
+
+  /** Initialize the internal state from a user-given value
+
+      Do not use the value 0 or the output is always 0
+  */
+  xorshift(const value_type& s)
+    : state { s }
+  {}
+
+
+  xorshift() = default;
+
 
   /// Compute a new pseudo random integer
-  const state_t& operator()() {
+  const value_type& operator()() {
     if constexpr (bit_size == 32) {
-      /* Pick the one of type I with best bit equidistribution from
+      /* Pick the one of type "I" with best bit equidistribution from
          Panneton & L'Écuyer, Section "5.1 Equidistribution
          properties"
 
@@ -69,14 +94,18 @@ struct xorshift {
       state ^= state >> 7;
       state ^= state << 17;
     }
-    else {
+    else if constexpr (bit_size == 128) {
       // The xor128 from Marsaglia, p. 5 Section "4 Summary"
-      auto t = state.x^(state.x<<11);
-      state.x = state.y;
-      state.y = state.z;
-      state.z = state.w;
-      state.w = state.w^(state.w>>19)^(t^(t>>8));
+      auto t = state[0]^(state[0]<<11);
+      state[0] = state[1];
+      state[1] = state[2];
+      state[2] = state[3];
+      state[3] = state[3]^(state[3]>>19)^(t^(t>>8));
     }
+    else
+      // Just to error in that case
+      return;
+
     return state;
   }
 
--- tests/random/xorshift.cpp
+++ tests/random/xorshift.cpp
@@ -2,9 +2,10 @@
 
    Exercise triSYCL sycl::vendor::trisycl::random::xorshift extension
 */
-#include <triSYCL/vendor/trisycl/random/xorshift.hpp>
 #include <iostream>
 #include <type_traits>
+
+#include <triSYCL/vendor/trisycl/random/xorshift.hpp>
 
 #include <boost/test/minimal.hpp>
 
@@ -15,18 +16,31 @@ int test_main(int argc, char *argv[]) {
   r::xorshift rng32;
   // Test default size
   static_assert(std::is_same_v<decltype(rng32), r::xorshift<32>>);
-  for(int i = 0; i != 20; ++i)
-    std::cout << "xor32\t" << i << ":\t" << std::hex << rng32() << std::endl;
+  BOOST_CHECK(rng32() == 0xb44f687c);
+  // for(int i = 0; i != 20; ++i)
+  //  std::cout << "xor32\t" << i << ":\t" << std::hex << rng32() << std::endl;
 
   r::xorshift<64> rng64;
-  for(int i = 0; i != 20; ++i)
-    std::cout << "xor64\t" << i << ":\t" << std::hex << rng64() << std::endl;
+  BOOST_CHECK(rng64() == 0x79690975fbde15b0);
+  // for(int i = 0; i != 20; ++i)
+  //  std::cout << "xor64\t" << i << ":\t" << std::hex << rng64() << std::endl;
 
   r::xorshift<128> rng128;
-  for(int i = 0; i != 20; ++i) {
-    auto [ a, b, c, d ] = rng128();
-    std::cout << "xor128\t" << i << ":\t" << std::hex << a << '\t' << b << '\t'
-              << c << '\t' << d << '\t' << std::endl;
-  }
+  BOOST_CHECK((rng128() == r::xorshift<128>::value_type
+      { 0x159a55e5, 0x1f123bb5, 0x5491333, 0xdca345ea }));
+  // for(int i = 0; i != 20; ++i) {
+  //   auto [ a, b, c, d ] = rng128();
+  //   std::cout << "xor128\t" << i << ":\t" << std::hex << a << '\t'
+  //               << b << '\t' << c << '\t' << d << '\t' << std::endl;
+  // }
+
+  r::xorshift<32> rng32_0 { 1 };
+  BOOST_CHECK(rng32_0() == 3 );
+
+  r::xorshift<64> rng64_0 { 1 };
+  BOOST_CHECK(rng64_0() == 0x40822041 );
+
+  r::xorshift<128> rng128_0 { { 1, 2, 3, 4 } };
+  BOOST_CHECK((rng128_0() == r::xorshift<128>::value_type { 2, 3, 4, 0x80d }));
   return 0;
 }
