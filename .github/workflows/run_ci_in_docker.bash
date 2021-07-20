9e9632ab4a4afae5ff67f58792cbf8b0e06f167b
Ronan Keryell <rkeryell@xilinx.com>
2021-07-20 08:57:13 -0700

    Use Docker to run the CI with a specific Ubuntu version
    Just try with g++-11 for now.

---
+++ .github/workflows/run_ci_in_docker.bash
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# Run the shell in verbose mode and show what is executed:
+set -vx
+
+echo Environment variables:
+env
+echo
+
+echo Identities:
+id
+echo
+
+echo Mounted file systems:
+df --human-readable
+echo
+
+echo Content of the home directory:
+ls -al ~
+echo
+
+# Options to install packages in an unattended way
+APT_ENABLE="--assume-yes --allow-downgrades \
+  --allow-remove-essential --allow-change-held-packages"
+# Do not ask question during the install because we cannot help
+export DEBIAN_FRONTEND=noninteractive
+
+# Get the latest list of packages
+apt-get update
+
+# Update the system to the latest packages
+apt-get dist-upgrade $APT_ENABLE
+
+# Install packages required by the CI
+apt-get install $APT_ENABLE apt-utils cmake libboost-all-dev \
+  librange-v3-dev
+
+# Install the required C compiler:
+apt-get install $APT_ENABLE $C_COMPILER
+
+# Install the required C++ compiler:
+apt-get install $APT_ENABLE $CXX_COMPILER
+
+# This is where the repository is mounted inside the Docker image
+cd /github/workspace
+
+echo Content of /github/workspace:
+ls -al
+echo
+
+# Configure triSYCL
+cmake . -DTRISYCL_OPENCL=$OPENCL -DTRISYCL_OPENMP=$OPENMP \
+  -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
+  && make -j`nproc`
+
+# Run the tests
+exec ctest
