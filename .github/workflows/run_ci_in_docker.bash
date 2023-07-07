bf0444ef9ad2b1c4df3ce3a0d2463fee31ebaa57
Ronan Keryell <rkeryell@xilinx.com>
2023-07-06 17:36:44 -0700

    Move Docker CI script logic into native GitHub action syntax
    This is to have predefined ccache GitHub action to work out-of-the box.

--- .github/workflows/run_ci_in_docker.bash
+++ .github/workflows/run_ci_in_docker.bash
@@ -1,5 +1,62 @@
 #! /bin/bash
 
+# This is no longer used as it has been retrofitted into the official GitHub action way
+# It used to be with:
+
+# jobs:
+
+#   docker:
+
+#     name: Run the validation inside Docker with specific OS and options
+
+#     runs-on: ubuntu-22.04
+
+#     strategy:
+#       # Run all the test even if there are some which fail
+#       fail-fast: false
+
+#       matrix:
+#        include:
+#          - c_compiler: gcc-12
+#            cxx_compiler: g++-12
+#            OpenMP: ON
+#            OpenCL: OFF
+
+#          - c_compiler: gcc-12
+#            cxx_compiler: g++-12
+#            OpenMP: ON
+#            OpenCL: ON
+
+#          - c_compiler: clang-17
+#            cxx_compiler: clang++-17
+#            OpenMP: ON
+#            OpenCL: OFF
+
+#          - c_compiler: clang-17
+#            cxx_compiler: clang++-17
+#            OpenMP: ON
+#            OpenCL: ON
+
+#     steps:
+#       - name: Check out repository code
+#         uses: actions/checkout@v3
+
+#       - name: ${{ matrix.c_compiler }}, ${{ matrix.cxx_compiler }},
+#           OpenMP=${{ matrix.OpenMP }}, OpenCL=${{ matrix.OpenCL }}
+#           in Docker Ubuntu 23.04
+#         uses: docker://ubuntu:lunar
+#         env:
+#           C_COMPILER: ${{ matrix.c_compiler }}
+#           CXX_COMPILER: ${{ matrix.cxx_compiler }}
+#           OPENMP: ${{ matrix.OpenMP }}
+#           OPENCL: ${{ matrix.OpenCL }}
+#         with:
+#           # The workspace is mounted at a different location inside Docker
+#           entrypoint: /github/workspace/.github/workflows/run_ci_in_docker.bash
+
+
+
+
 # Run the shell in verbose mode, show what is executed
 # and exit on the first error:
 set -vxe
